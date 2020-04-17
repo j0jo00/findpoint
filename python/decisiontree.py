87a8897d7613610c4c7ad98a1fc94f29bfb43ef8
stephe93n <zouyangtiti@outlook.com>
2020-04-18 00:15:51 +0800

    add python

---
+++ python/decisiontree.py
@@ -0,0 +1,12 @@
+
+
+# 构造树
+# dataset 输入的数据集
+# classes 数据集中数据对应的类型
+# feature_names 数据集中数据相应的特征名称
+
+def create_tree(self,dataset,classes,feature_names):
+    pass
+
+if __name__ == "__main__":
+    print("decision tree")
